5e1e07ed52041737729063e212cd22051d1e1442
S <guerreqq@gmail.com>
2023-01-25 02:59:24 +0800

    update version

--- guess-num.py
+++ guess-num.py
@@ -7,13 +7,17 @@
 import random 
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	g = input('請猜數字:')
 	g = int(g)
 	if g == r:
 		print('你猜對了')
+		print('這是你猜的第', count, '次')
 		break
 	elif g > r:
 		print('答案比你猜的小')
 	elif g < r:
 		print('答案比你猜的大')
+	print('這是你猜的第', count, '次')
